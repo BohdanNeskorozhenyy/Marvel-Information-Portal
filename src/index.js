c5f767c1c83a6105b1c06b50d5f204328ea2abcb
Bohdan <bogdaneskor@gmail.com>
2021-11-30 17:21:39 +0200

    add services

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelServices from './services/MarvelService';
+
+
 import './style/style.scss';
+
+const marvelService = new MarvelServices();
+
+marvelService.getAllCharacters().then(response => response.data.results.forEach(item => console.log(item.name)));
 
 ReactDOM.render(
   <React.StrictMode>
