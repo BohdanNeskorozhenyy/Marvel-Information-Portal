6e1f757938ede9a7d8ac25a5f34c62fc4c51e19e
Bohdan <bogdaneskor@gmail.com>
2021-12-08 13:29:38 +0200

    make it better

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -3,16 +3,16 @@ import './appHeader.scss';
 const AppHeader = () => {
     return (
         <header className="app__header">
-            <h1 style={{fontSize:'40px'}} className="app__title">
-                <a style={{color:'#DE3535'}} href="#">
-                    <span style={{color:'#DE3535'}}>Marvel</span> information portal
+            <h1 className="app__title">
+                <a  href="#">
+                    <span>Marvel</span> information portal
                 </a>
             </h1>
             <nav className="app__menu">
-                <ul style={{color:'white',fontSize:'30px'}}>
-                    <li><a style={{color:'white'}} href="#">Characters</a></li>
+                <ul>
+                    <li><a  href="#">Characters</a></li>
                     /
-                    <li><a style={{color:'white'}} href="#">Comics</a></li>
+                    <li><a href="#">Comics</a></li>
                 </ul>
             </nav>
         </header>
